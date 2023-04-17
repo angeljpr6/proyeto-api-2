91856f2271457a92be33d0716bcac30e9652029b
angeljpr6 <angelpr.dam@gmail.com>
2023-04-17 17:18:16 +0100

    segundo commit

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 var express=require ('express');
-var app=express;
-//a
+var app=express();
+
+app.get('/', function (req, res){
+    res.send('Hola Mundo');
+});
+
+app.get('/abouttime', function (req, res){
+    res.json({
+        'name':'Teclado',
+        'precio':22,
+        'category':'tecnology'
+    })
+});
+
+app.listen(3000, function(){
+    console.log('Aplicacion de ejemplo');
+});
