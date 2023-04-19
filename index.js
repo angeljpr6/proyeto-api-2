251fc40aaa7802b9910f61874b2e2d28724fca0b
angeljpr6 <angelpr.dam@gmail.com>
2023-04-19 18:47:43 +0100

    cuarto commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Arregla el abouttime que estaba mal
 var express=require ('express');
 var app=express();
 
@@ -5,14 +6,19 @@ app.get('/', function (req, res){
     res.send('Hola Mundo');
 });
 
-app.get('/abouttime', function (req, res){
+ap
+
+app.get('/products', function (req, res){
     res.json({
         'name':'Teclado',
         'precio':22,
         'category':'tecnology'
     })
 });
-
 app.listen(3000, function(){
-    console.log('Aplicacion de ejemplo');
+    console.log('Aplicacion de ejemplo escuchando en el puerto 3000');
 });
+app.get('/abouttime', function (req, res){
+    res.send("pruebas de api");
+});
+
