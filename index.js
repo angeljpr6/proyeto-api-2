95ad7c75e5ccb5d3987fe0d95ee2aa06fefc22cb
angeljpr6 <angelpr.dam@gmail.com>
2023-04-21 17:02:45 +0100

    productos con id

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 var express=require ('express');
 var app=express();
 
+
 app.get('/', function (req, res){
-    res.send('Hola Mundo');
+    res.send('Hola Mundoo');
 });
 
 
@@ -14,6 +15,27 @@ app.get('/products', function (req, res){
         'category':'tecnology'
     })
 });
+app.get('/users', function (req,res){
+    const [limit,offset]=req.query;
+    if (limit && offset){
+        res.json({
+            limit,
+            offset
+        })
+    }else{
+        res.send("No has pasado los parametros adecuados")
+    }
+})
+app.get('/products/:id', function (req, res){
+    let (id)=req.params;
+    res.json({
+        'id' : id,
+        'name' : 'Teclado',
+        'precio' : 22,
+        'category' : 'todos los productos'
+    })
+});
+
 app.listen(3000, function(){
     console.log('Aplicacion de ejemplo escuchando en el puerto 3000');
 });
